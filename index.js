e185fa4c2c8cd1465118babc09d14b3d056e10d0
Costantino-Scardigno <costantino98@icloud.com>
2024-11-19 19:14:21 +0100

    agg

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 const url = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=fabri%20fibra'
 
-const getApi
+const homePage = function () {
+    fetch(url, {
+        method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'afa581262bmsh759f16f17696ca1p195c63jsn334671c1c2a8',
+		'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+    })
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Errore richiesta");
+        }
+        
+    })
+
+    .then((data)=> {
+        const imgBanner =document.getElementById("imgAlbum")
+        imgBanner.src= data.data[0].album.cover_big
+        const h1Banner=document.getElementById("h1banner")
+        h1Banner.innerText= data.data[0].album.title
+        const artistbanner =document.getElementById("artistbanner")
+        artistbanner.innerText= data.data[0].artist.name
+    })
+
+
+
+
+
+    .catch((error) => {
+        console.log(error);
+    });
+
+}
+
+window.onload = () => {
+    homePage();
+};
+
